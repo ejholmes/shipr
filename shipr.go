69b1e9c5a0541bae28189db9d22d3152bfba97c8
Eric J. Holmes <eric@ejholmes.net>
2014-07-08 22:22:15 -0700

    Comment.

--- shipr.go
+++ shipr.go
@@ -39,7 +39,7 @@ func New(path, env string) (*Shipr, error) {
 	}, nil
 }
 
-// Deploy is the primary interface into deploy things. It takes an object that conforms
+// Deploy is the primary interface into deploying things. It takes an object that conforms
 // to the Deployment interface, creates a Job then runs it.
 func (c *Shipr) Deploy(d Deployment) error {
 	j, err := c.Jobs.CreateFromDeployment(d)
