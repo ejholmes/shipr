78b55b4e396a297462eeb8ce35eb93ce4d617ebf
Eric J. Holmes <eric@ejholmes.net>
2014-07-14 08:34:30 -0700

    Cleanup.

--- server/github/github.go
+++ server/github/github.go
@@ -7,61 +7,31 @@ import (
 	"github.com/ejholmes/go-github/github"
 	"github.com/gorilla/mux"
 	"github.com/remind101/shipr"
-	"github.com/remind101/shipr/util"
 )
 
-const GitHubEventHeader = "X-GitHub-Event"
+const EventHeader = "X-GitHub-Event"
 
-type GitHubEventHandler http.Handler
+type EventHandler http.Handler
 
-// GitHubHandler demuxes incoming webhooks from GitHub and handles them.
-type GitHubHandler struct {
+// Handler demuxes incoming webhooks from GitHub and handles them.
+type Handler struct {
 	http.Handler
 }
 
-func New(c *shipr.Shipr) *GitHubHandler {
+func New(c *shipr.Shipr) http.Handler {
 	m := mux.NewRouter()
-	h := &GitHubHandler{m}
+	h := &Handler{m}
 
-	var handlers = map[string]GitHubEventHandler{
+	var handlers = map[string]EventHandler{
 		"deployment":        &DeploymentHandler{c},
 		"deployment_status": &DeploymentStatusHandler{c},
 	}
 
 	for event, handler := range handlers {
-		m.Methods("POST").Headers(GitHubEventHeader, event).Handler(handler)
+		m.Methods("POST").Headers(EventHeader, event).Handler(handler)
 	}
 
 	return h
-}
-
-// description wraps a github.Deployment to implement the shipr.Description interface.
-type description struct {
-	*github.Deployment
-}
-
-func (d *description) Guid() int {
-	return *d.Deployment.ID
-}
-
-func (d *description) RepoName() shipr.RepoName {
-	return shipr.RepoName(util.SafeString(d.Deployment.Repository.FullName))
-}
-
-func (d *description) Sha() string {
-	return util.SafeString(d.Deployment.Sha)
-}
-
-func (d *description) Ref() string {
-	return util.SafeString(d.Deployment.Ref)
-}
-
-func (d *description) Environment() string {
-	return util.SafeString(d.Deployment.Environment)
-}
-
-func (d *description) Description() string {
-	return util.SafeString(d.Deployment.Description)
 }
 
 type DeploymentHandler struct {
