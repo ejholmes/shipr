6844d4374886e9c10299014f3d2134a252ef94df
Eric J. Holmes <eric@ejholmes.net>
2014-07-20 10:37:55 -0700

    Rev.

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,11 +1,22 @@
 var gulp = require('gulp')
     concat = require('gulp-concat'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify')
+    rev = require('gulp-rev');
+
+var build = '../server/frontend';
 
 gulp.task('javascripts', function() {
   return gulp.src('javascripts/**/*.js')
     .pipe(concat('app.js'))
-    .pipe(gulp.dest('../server/frontend'));
+    .pipe(gulp.dest(build));
+});
+
+gulp.task('rev', ['javascripts'], function() {
+  return gulp.src([build + '**/*.js'])
+    .pipe(rev())
+    .pipe(gulp.dest(build))
+    .pipe(rev.manifest())
+    .pipe(gulp.dest(build))
 });
 
 gulp.task('watch', ['javascripts'], function() {
