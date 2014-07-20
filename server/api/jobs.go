a26e34222802979ea29a05174432aade6c6409b6
Eric J. Holmes <eric@ejholmes.net>
2014-07-20 16:09:55 -0700

    Cleanup.

--- server/api/jobs.go
+++ server/api/jobs.go
@@ -6,7 +6,8 @@ import (
 	"github.com/remind101/shipr"
 )
 
-type JobResource struct {
+// job is the api representation of a shipr.Job.
+type job struct {
 	ID          int     `json:"id"`
 	Guid        int     `json:"guid"`
 	Sha         string  `json:"sha"`
@@ -18,8 +19,9 @@ type JobResource struct {
 	Output      *string `json:"output,omitempty"`
 }
 
-func NewJobResource(j *shipr.Job) *JobResource {
-	return &JobResource{
+// Job decorates a shipr.Job as a Job resource.
+func Job(j *shipr.Job) *job {
+	return &job{
 		ID:          j.ID,
 		Guid:        j.Guid,
 		Sha:         j.Sha,
@@ -30,40 +32,47 @@ func NewJobResource(j *shipr.Job) *JobResource {
 	}
 }
 
+// Jobs decorates a slice of shipr.Job as a Job resource.
+func Jobs(jobs []*shipr.Job) []*job {
+	r := make([]*job, len(jobs))
+	for i, j := range jobs {
+		r[i] = Job(j)
+	}
+	return r
+}
+
+// JobsList responds with all jobs.
 func JobsList(c *shipr.Shipr, res *Response, req *Request) {
 	jobs, err := c.Jobs.All()
 	if err != nil {
 		panic(err)
 	}
-	r := make([]*JobResource, len(jobs))
-	for i, j := range jobs {
-		r[i] = NewJobResource(j)
-	}
 	res.Status(200)
-	res.Present(r)
+	res.Present(Jobs(jobs))
 }
 
+// JobsInfo responds with a single job.
 func JobsInfo(c *shipr.Shipr, res *Response, req *Request) {
 	id, err := strconv.Atoi(req.Var("id"))
 	if err != nil {
 		panic(err)
 	}
-	job, err := c.Jobs.Find(id)
+	j, err := c.Jobs.Find(id)
 	if err != nil {
 		panic(err)
 	}
 
-	if job == nil {
+	if j == nil {
 		res.NotFound()
 		return
 	}
 
-	output, err := c.LogLines.Output(job)
+	output, err := c.LogLines.Output(j)
 	if err != nil {
 		panic(err)
 	}
 
-	resource := NewJobResource(job)
+	resource := Job(j)
 	resource.Output = &output
 
 	res.Status(200)
